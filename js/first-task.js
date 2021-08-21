4b79e42e50783d59a3f5891a6b5fdb5d43f81b9e
Nor10ava <anastasiatasyarom@gmail.com>
2021-08-21 18:29:49 +0100

    base

--- js/first-task.js
+++ js/first-task.js
@@ -1,10 +1,13 @@
 const delay = (ms) => {
-  // Твой код
+  // Твой
+  return new Promise((resolve) => {
+    setTimeout(() => resolve(ms), ms);
+  });
 };
 
-const logger = (time) => console.log(`Resolved after ${time}ms`);
+// const logger = (time) => console.log(`Resolved after ${time}ms`);
 
 // Вызовы функции для проверки
-delay(2000).then(logger); // Resolved after 2000ms
-delay(1000).then(logger); // Resolved after 1000ms
-delay(1500).then(logger); // Resolved after 1500ms
+delay(2000).then((ms) => console.log(`Resolved after ${ms}ms`)); // Resolved after 2000ms
+delay(1000).then((ms) => console.log(`Resolved after ${ms}ms`)); // Resolved after 1000ms
+delay(1500).then((ms) => console.log(`Resolved after ${ms}ms`)); // Resolved after 1500ms
